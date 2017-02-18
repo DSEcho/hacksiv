f5489e969e13a4537c2e74990f7bc8411db0a693
dedawgtubby <darencheng1@gmail.com>
2017-02-18 11:05:58 -0500

    Commented snek
    beep

--- Project1/Project1/Snek.cpp
+++ Project1/Project1/Snek.cpp
@@ -3,24 +3,52 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+To be called after we move.
+returns true if collides with wall, self, and food
+*/
 template <class ItemType>
 bool Snek::collide()
 {
 
 }
 
+
+
+/*
+we will be controlling all the positions of the linked list
+
+alex is writing something that will traverse a link list.
+
+
+eventually will call logic on chalk board
+*/
 template <class ItemType>
 void Snek::move(Direction)
 {
 
 }
 
+
+/*
+if size some number we want and win
+
+*/
 template <class ItemType>
 bool Snek::hasWon()
 {
 
 }
 
+
+/*
+will just create a new object at end.
+
+will be confusing, because we will be adding and subtracting 
+snek body parts.
+
+*/
 template <class ItemType>
 void Snek::grow()
 {
@@ -28,14 +56,11 @@ void Snek::grow()
 
 }
 
+/*
+returns the current size minus default size
+*/
 template <class ItemType>
 int Snek::getScore()
-{
-
-}
-
-template <class ItemType>
-void Snek::move(int)
 {
 
 }
