ab2a8f81ac95e184086682575831795040a2a71d
Daggem S <daggem.a.samuel@gmail.com>
2017-02-18 11:42:26 -0500

    Snek collide stuffs

--- Project1/Project1/Snek.cpp
+++ Project1/Project1/Snek.cpp
@@ -11,7 +11,37 @@ returns true if collides with wall, self, and food
 template <class ItemType>
 bool Snek::collide()
 {
+	Node<ItemType> *curPtr = headptr;
 
+	curPtr = curPtr->getNext();
+	bool hit = false;
+
+	if (x >= 50 || y >= 50 || z >= 50 || x < 0 || y < 0 || z < 0)
+	{
+		hit = true;
+	}
+	for (curPtr; curPtr != nullptr && !hit; curPtr = curPtr->getNext())
+	{// ranges from 0 - 2
+		// 0 is x
+		// 1 is y
+		// 2 is z
+		int countOfXYZMatch = 0;
+		for (int i = 0; i < 3; i++)
+		{
+
+			if (*curPtr[i] == *headptr[i])
+			{
+				countOfXYZMatch++;
+			}
+		}
+		if (countOfXYZMatch == 3)
+		{
+			hit = true;
+		}
+		countOfXYZMatch = 0;
+	}//have checked if hitted self.
+
+	return hit;
 }
 
 
